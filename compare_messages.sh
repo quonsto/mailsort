a7d4b6be9d3595c3e888af546788140760657521
Konstantin Solodovnikov <k.solodovnikov@ngenix.net>
2015-03-20 21:11:30 +0400

    При сравнении сообщений игнорируем заголовок Keywords.

--- compare_messages.sh
+++ compare_messages.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-diff -uw -I '^X-OlkEid:' -I '^x-originalarrivaltime:' -I '^x-bayesian-words:' -I '^x-bayesian-result:' <(unify_message.sh "$1") <(unify_message.sh "$2")
+diff -uw -I '^X-OlkEid:' -I '^x-originalarrivaltime:' -I '^x-bayesian-words:' -I '^x-bayesian-result:' -I '^Keywords:' <(unify_message.sh "$1") <(unify_message.sh "$2")
