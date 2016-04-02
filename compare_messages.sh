94a15fa4c28451a7efa002f478dbe5dcb78c6fa5
Konstantin Solodovnikov <k.solodovnikov@ngenix.net>
2016-04-02 13:11:47 +0400

    более строго определяем заголовки.

--- compare_messages.sh
+++ compare_messages.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 diff -uw \
-    -I '^X-OlkEid:' \
-    -I '^x-originalarrivaltime:' \
-    -I '^x-bayesian-words:' \
-    -I '^x-bayesian-result:' \
-    -I '^Keywords:' \
-    -I '^x-spampal:' \
-    -I '^x-mimeole:' \
-    -I '^Return-Path:' \
+    -I '^X-OlkEid: ' \
+    -I '^x-originalarrivaltime: ' \
+    -I '^x-bayesian-words: ' \
+    -I '^x-bayesian-result: ' \
+    -I '^Keywords: ' \
+    -I '^x-spampal: ' \
+    -I '^x-mimeole: ' \
+    -I '^Return-Path: ' \
     -I '^.*----=_NextPart_' \
-    -I '^x-felis-deliver-id:' \
-    -I '^x-sender:' \
+    -I '^x-felis-deliver-id: ' \
+    -I '^x-sender: ' \
   <(unify_message.sh "$1") <(unify_message.sh "$2")
