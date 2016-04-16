fc1f10cffd2e60c673583b4821134a9f39aaf47c
Konstantin Solodovnikov <k.solodovnikov@ngenix.net>
2016-04-17 01:24:22 +0400

    Попарное сравнение ещё двух папок.

--- dispatch_message.pl
+++ dispatch_message.pl
@@ -170,6 +170,9 @@ use vars qw/%IS_BETTER_THAN/; BEGIN {
 %IS_BETTER_THAN = (
   "Professional/Work/Archive/Telecom-Centre/Maintenance/САСП/Распоряжения и заявки" => {
     "Переписка с клиентами" => 1
+  },
+  "Professional/Work/Archive/Telecom-Centre/Significant &- Important" => {
+    "Significant &- Important" => 1
   }
 );
 
