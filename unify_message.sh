b3af222c6feb8dcdc903d8d1d091acd082ba4009
Konstantin Solodovnikov <k.solodovnikov@ngenix.net>
2016-04-18 01:32:22 +0400

    Игнорируем содержимое аттачмента winmail.dat при сравнении файлов.
    ... но не факт его наличия!

--- unify_message.sh
+++ unify_message.sh
@@ -4,4 +4,10 @@ decode_message.pl "$1" |
    perl -pe '
        s/^Subject: \*\*SPAM\*\*/Subject: /g;
        s/^Subject: :SPAM:/Subject: /g
+   ' | 
+   perl -ne '
+       /boundary="(.*)"/ and $boundary = $1;
+       $boundary and /^--$boundary--$/ and $tnef_mode = 0;
+       print unless $tnef_mode;
+       /^\s+filename="winmail.dat"$/ and $tnef_mode = 1;
    '
