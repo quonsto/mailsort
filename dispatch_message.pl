393a52db5e3b1c21ed34265deb9cd8c1a3d7c460
Konstantin Solodovnikov <k.solodovnikov@ngenix.net>
2016-04-19 14:35:22 +0400

    Ещё одна спамовская папка.

--- dispatch_message.pl
+++ dispatch_message.pl
@@ -173,6 +173,7 @@ use vars qw/%FOLDERS_PREFERENCE/; BEGIN {
   "Personal/Fun" => 750,
   "Inbox/! Inbox до разделения папок" => 999,
   "Inbox/Spam/ManuallyIdentidied" => 1000,
+  "Inbox/Spam/AutoIdentified" => 1000,
   "Inbox/Spam/Old" => 1000,
   "Inbox/Spam reports" => 1000,
   "Входящие/Spam reports" => 1001,
