03d8dc84ff9d757413ac77a6d1fd3f665cd23e3b
Konstantin Solodovnikov <k.solodovnikov@ngenix.net>
2016-04-23 22:18:10 +0400

    С несколько бОльшим снисхождением относимся к задержкам при поиске файлов в папках.

--- dispatch_messages.sh
+++ dispatch_messages.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-find ../byid -name files | while read -t 1 file; do
+find ../byid -name files | while read -t 5 file; do
   #echo $file | perl -pe 's/\n/\0/g' | xargs -0 -n 1 cat | 
   cat $file |
     ( dispatch_message.pl && rm $file ) || ( echo $file; exit 1 ) || exit 1
